de2c0dff4db6ee09fbfc066d32eead8cd4bab260
Meabhijit11 <meabhijitmeshram@gmail.com>
2023-07-30 18:34:37 +0530

    dvc added

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path('config/config.ymal')
-PARAMS_FILE_PATH = Path('params.ymal')
+CONFIG_FILE_PATH = Path('config/config.yaml')
+PARAMS_FILE_PATH = Path('params.yaml')
